310792286280c946d4d0171e321377fcf449d14e
gluknik <gluknik@gmail.com>
2022-09-19 23:12:30 +0300

    Fix for vh in mobile

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,14 @@
 import About from './sections/About'
 
 export default function () {
+  const appHeight = function appHeight() {
+    const doc = document.documentElement
+    doc.style.setProperty('--app-height', ''.concat(window.innerHeight + 'px'))
+  }
+
+  window.addEventListener('resize', appHeight)
+  appHeight()
+
   return (
     <>
       <About />
