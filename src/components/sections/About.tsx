361071017256c6915065456792dabc9a254ca34a
gluknik <gluknik@gmail.com>
2022-09-19 23:04:12 +0300

    Fix

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -7,7 +7,7 @@ export default function () {
       </h1>
       <h2>I build things for the web.</h2>
       <p class="short-desc">
-        I’m a software engineer specializing in building exceptional digital
+        I’m a web developer specializing in building exceptional digital
         experiences. Currently, I’m focused on building accessible,
         human-centered products at{' '}
         <a href="https://www.richkid.co.il/" target="_blank">
